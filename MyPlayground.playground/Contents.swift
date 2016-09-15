6b9f9d4359049f84fdf7d3af6476b4c36ac2d2fc
JohnathanFlint <nicholas.white142@gmail.com>
2016-09-15 09:33:22 -0600

    First Swift playground.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var feeding = "Did you remember to feed Taco?"
+print(feeding)
+
+let theTacoMonster = true
+let hunger = false
+
+if hunger {
+    print("😈")
+}
+else {
+    print("😝")
+}
+
